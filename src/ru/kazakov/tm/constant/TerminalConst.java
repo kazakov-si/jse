755184b908471071d8686f638f36ee03caa1fcfa
Sergey Kazakov <kazak605@gmail.com>
2020-07-10 12:34:07 +0300

    Добавлен класс с константами терминальных команд и произведен рефакторинг на блок case.

---
+++ src/ru/kazakov/tm/constant/TerminalConst.java
@@ -0,0 +1,8 @@
+package ru.kazakov.tm.constant;
+
+public class TerminalConst {
+
+    public static final String HELP = "help";
+    public static final String VERSION = "version";
+    public static final String ABOUT = "about";
+}
