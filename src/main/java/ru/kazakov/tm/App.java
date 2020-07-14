e1f6cbc765035cd8ac72e8963d43abca962d2c1a
Sergey Kazakov <kazak605@gmail.com>
2020-07-14 14:16:51 +0300

    Добавлен Apache Maven

--- src/main/java/ru/kazakov/tm/App.java
+++ src/main/java/ru/kazakov/tm/App.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 import static ru.kazakov.tm.constant.TerminalConst.*;
 
-public class Main {
+public class App {
 
     public static void main(String[] args) {
         run(args);
