0f2778daa7473b5bb0e97ab82068aa4d23e8a2d5
Sergey Kazakov <kazak605@gmail.com>
2020-07-16 15:03:07 +0300

    Добавлен консольный ввод параметров.

--- src/main/java/ru/kazakov/tm/App.java
+++ src/main/java/ru/kazakov/tm/App.java
@@ -1,6 +1,7 @@
 package ru.kazakov.tm;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 import static ru.kazakov.tm.constant.TerminalConst.*;
 
@@ -9,44 +10,73 @@ public class App {
     public static void main(String[] args) {
         run(args);
         displayWelcome();
-    }
-
-    private static void run (final String[] args) {
-        if (args == null) return;
-        if (args.length < 1) return;
-        final String param = args[0];
-        switch (param) {
-            case VERSION: displayVersion();
-            case ABOUT: displayAbout();
-            case HELP: displayHelp();
-            default: displayError();
+        final Scanner scanner = new Scanner(System.in);
+        String command = "";
+        while (!Exit.equals(command)) {
+            command = scanner.nextLine();
+            run(command);
         }
     }
 
-    private static void displayError() {
+    private static void run(final String[] args) {
+        if (args == null) return;
+        if (args.length < 1) return;
+        final String param = args[0];
+        final int result = run(param);
+        System.exit(result);
+    }
+
+    private static int run(final String param) {
+        if (param == null) return -1;
+        switch (param) {
+            case VERSION:
+                return displayVersion();
+            case ABOUT:
+                return displayAbout();
+            case HELP:
+                return displayHelp();
+            case Exit:
+                return displayExit();
+            default:
+                return displayError();
+        }
+    }
+
+    private static int displayExit() {
+        System.out.println("Terminate program...");
+        return 0;
+        //System.exit(-1);
+    }
+
+    private static int displayError() {
         System.out.println("Error! Unknown program argument...");
-        System.exit(-1);
+        return -1;
+        //System.exit(-1);
     }
 
     private static void displayWelcome() {
         System.out.println("** WELCOME TO TASK MANAGER **");
     }
 
-    private static void displayHelp() {
+    private static int displayHelp() {
         System.out.println("version - Display application version.");
         System.out.println("about - Display developer info.");
         System.out.println("help - Display list of commands.");
-        System.exit(0);
+        System.out.println("exit - Terminate console application.");
+        return 0;
+        //System.exit(0);
     }
 
-    private static void displayVersion() {
+    private static int displayVersion() {
         System.out.println("1.0.0");
-        System.exit(0);
-     }
+        return 0;
+        //System.exit(0);
+    }
 
-     private static void displayAbout() {
-         System.out.println("Kazakov Sergey");
-         System.out.println("kazakov_si@nlmk.com");
-         System.exit(0);
-     }
+    private static int displayAbout() {
+        System.out.println("Kazakov Sergey");
+        System.out.println("kazakov_si@nlmk.com");
+        return 0;
+        //System.exit(0);
+    }
 }
